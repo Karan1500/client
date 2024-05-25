2bccd0f3cff8a0af74926659f3126bbde2b8b2b0
Karan Jivanramjiwala <karanhj786@gmail.com>
2024-05-26 00:35:30 +0530

    room pop-up added

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Carousel from 'react-bootstrap/Carousel';
 
 function Room({room}){
+
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     return(
         <div className="row bs">
             <div className="col-md-4">
@@ -13,12 +22,34 @@ function Room({room}){
                 <p>Room Type: {room.Type}</p>
 
                 <div style={{float: 'right'}}>
-                    <button className="btn btn-primary">View Details</button>
+                    <button className="btn btn-primary" onClick={handleShow}>View Details</button>
                 </div>
                 <div style={{float: 'right'}}>
                     <button className="btn btn-primary">Book Now</button>
                 </div>
             </div>
+
+            <Modal show={show} onHide={handleClose} size="lg">
+                <Modal.Header>
+                <Modal.Title>{room.name}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                <Carousel prevLabel='' nextLabel=''>
+                        {room.imageUrls.map(url=>{
+                            return <Carousel.Item>
+                            <img className="d-block w-100 bigimg" src={url} />
+                          </Carousel.Item>
+                        })}
+                </Carousel>
+                <p>{room.description}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+                </Modal.Footer>
+            </Modal>
+
         </div>
     )
 };
