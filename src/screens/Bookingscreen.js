b5a3e2b731928c108ba9146d53a7630a7aeeb2bc
Karan Jivanramjiwala <karanhj786@gmail.com>
2024-05-26 16:20:24 +0530

    Booking screen frontend added

--- src/screens/Bookingscreen.js
+++ src/screens/Bookingscreen.js
@@ -1,13 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
 import { useParams } from 'react-router-dom';
+import Loader from '../components/Loader';
+import Error from '../components/Error';
 
 function Bookingscreen() {
     let { roomid } = useParams(); // Destructure the roomid from the useParams object
     // console.log(roomid); // roomid should now be a string or undefined
 
-    const [rooms, setroom] = useState();
-    const [loading, setloading] = useState();
+    const [room, setroom] = useState();
+    const [loading, setloading] = useState(true);
     const [error, seterror] = useState();
 
     useEffect(() => {
@@ -27,9 +29,40 @@ function Bookingscreen() {
       }, []); 
 
     return (
-        <div>
-            <h1>Booking Screen</h1>
-            <h1>Room Id = {roomid}</h1>
+        <div style={{margin: '50px'}}>
+            {loading ? (<Loader/>) : room ? (<div>
+                <div className='row justify-content-center mt-5 bs'>
+                    <div className='col-md-6'>
+                        <h1>{room.name}</h1>
+                        <img src={room.imageUrls[0]} className='bigimg'/>
+                    </div>
+                    <div className='col-md-6'>
+                        <div style={{textAlign: 'right'}}>
+                            <h1>Booking Details</h1>
+                            <hr/>
+                            <b>
+                                <p>Name : </p>
+                                <p>From Date : </p>
+                                <p>To Date : </p>
+                                <p>Max Count : {room.maxCount}</p>
+                            </b>
+                        </div>
+                        <div style={{textAlign: 'right'}}>
+                            <h1>Amount</h1>
+                            <hr/>
+                            <b>
+                                <p>Total days : </p>
+                                <p>Rent per day : {room.rentPerDay}</p>
+                                <p>Total Amount : </p>
+                            </b>
+                        </div>
+                        <div style={{float: 'right'}}>
+                            <button className='btn btn-primary'>Pay Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            ): (<Error/>)}
         </div>
     );
 }
